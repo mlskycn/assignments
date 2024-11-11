c12e6991790a78d2fc0f663f55f803b5d33be4b5
Mlskycn <meliiskayacan@gmail.com>
2024-11-11 12:09:01 -0500

    check my assignment

---
+++ assignment9.py
@@ -0,0 +1,9 @@
+name = "Melis"
+password = "M@123"
+
+first_name = input("Enter your name: ")
+
+if first_name == name:
+    print(f"Hello, {first_name}! The password is : {password}")
+else:
+    print(f"Hello, {first_name}! See you later")
